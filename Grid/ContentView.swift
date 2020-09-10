c8ceedc5f2a995fd3d6b2584978d0e952749bd8d
Rob Bishop <c.sagan.ritual@gmail.com>
2020-09-10 12:33:33 -0700

    Initial Commit

---
+++ Grid/ContentView.swift
@@ -0,0 +1,17 @@
+// We are a way for the cosmos to know itself. -- C. Sagan
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
